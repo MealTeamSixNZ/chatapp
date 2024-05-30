c671f2388b55f2ba09249b00b546f68de34deec5
MealTeamSixNZ <github@kiwiorbit.com>
2024-05-31 06:33:03 +1000

    added basic authentication

--- backend/index.ts
+++ backend/index.ts
@@ -3,8 +3,11 @@ import express from "express"
 const app = express()
 const port = 3000
 const messageService = new MessageService()
+const USERNAME = "admin"
+const PASSWORD = "admin"
 
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 
 app.get(`/msgs`, (req, res) => {
@@ -19,6 +22,21 @@ app.post(`/msgs`, (req, res) => {
         messageService.createMessage(req.body.content)
         res.send()
     }
+})
+
+app.post(`/auth/sign_in`, (req, res) => {
+    console.log(req.body)
+    if (req.body.username === USERNAME && req.body.password === PASSWORD) {
+        res.cookie("username", USERNAME)
+        res.redirect("/")
+    } else {
+        res.redirect("/login.html")
+    }
+})
+
+app.get(`/auth/sign_out`, (req, res) => {
+    res.clearCookie("username")
+    res.redirect("/login.html")
 })
 
 app.listen(port, () => {
