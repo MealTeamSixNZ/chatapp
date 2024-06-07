ff1c90ba0d8d91815deab5783e6758d7fd05992c
MealTeamSixNZ <github@kiwiorbit.com>
2024-06-08 11:00:17 +1200

    add persistent sessions

--- backend/SessionService.ts
+++ backend/SessionService.ts
@@ -1,17 +1,37 @@
-import User from "./User"
+const SESSION_LOG = "sessions.json"
+import { json } from "express"
+import fs from "fs"
 
 class SessionService {
+    private readsSessionsFromFile():void {
+        if (!fs.existsSync(SESSION_LOG)) {
+            return
+        }
+        const jason = fs.readFileSync(SESSION_LOG, {encoding:"utf-8"})
+        const array = JSON.parse(jason)
+        this.sessions = new Map(array)
+    }
+    private writeSessionsToFile():void {
+        const entries = this.sessions.entries()
+        const array = Array.from(entries)
+        const jason = JSON.stringify(array)
+        fs.writeFileSync(SESSION_LOG, jason)
+    }
+    
     private sessions: Map<string, string>
     constructor() {
        this.sessions = new Map()
+       this.readsSessionsFromFile()
     }
     create(username: string):SessionID {
         const sessionID = crypto.randomUUID()
         this.sessions.set(sessionID, username)
+        this.writeSessionsToFile()
         return sessionID
     }
     revoke(sessionID: SessionID):void {
         this.sessions.delete(sessionID)
+        this.writeSessionsToFile()
     }
     find(sessionID: SessionID):string {
         const username = this.sessions.get(sessionID)
